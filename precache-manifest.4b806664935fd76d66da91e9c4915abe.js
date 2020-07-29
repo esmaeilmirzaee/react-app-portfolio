46c91538f130bd01ce44062a8bbc6678bcb3b7e8
EsmaeilMIRZAEE <EsmaeilMIRZAEE@gmail.com>
2020-07-29 18:38:00 -0400

    Updates

---
+++ precache-manifest.4b806664935fd76d66da91e9c4915abe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb25b24054421c8d38e0a92d7d77150",
+    "url": "/react-app-portfolio/index.html"
+  },
+  {
+    "revision": "b460bd4ebab906a2e0c1",
+    "url": "/react-app-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6b9b3c4f58b5991aea82",
+    "url": "/react-app-portfolio/static/js/2.12558916.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-portfolio/static/js/2.12558916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b460bd4ebab906a2e0c1",
+    "url": "/react-app-portfolio/static/js/main.322e38a5.chunk.js"
+  },
+  {
+    "revision": "058eeb1361a52f64764c",
+    "url": "/react-app-portfolio/static/js/runtime-main.f20a4723.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
